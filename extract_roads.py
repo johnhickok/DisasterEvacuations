cd328ad71eeca74dbea3ef25860971b97cf90ae7
John Hickok <jhickok2011@gmail.com>
2017-12-18 19:36:52 -0800

    tested and uploaded script
    uploaded

---
+++ extract_roads.py
@@ -0,0 +1,32 @@
+# This Python 2.x script assumes 
+# - you downloaded california-latest-free.shp.zip # from Geofabrik.
+# (http://download.geofabrik.de/north-america/us/california-latest-free.shp.zip)
+# - You used the OSGEO4W installation of QGIS
+
+import os, zipfile, subprocess
+
+#Set the current working folder
+thisfolder = os.getcwd()
+
+# Extracts only the roads shapefile
+geofabrik = zipfile.ZipFile("california-latest-free.shp.zip")
+geofabrik.extract("gis.osm_roads_free_1.cpg")
+geofabrik.extract("gis.osm_roads_free_1.dbf")
+geofabrik.extract("gis.osm_roads_free_1.prj")
+geofabrik.extract("gis.osm_roads_free_1.shp")
+geofabrik.extract("gis.osm_roads_free_1.shx")
+
+geofabrik.close()
+
+# rename the shapefile to prep for geoprocessing
+os.rename('gis.osm_roads_free_1.cpg', 'osm_roads_free_1.cpg')
+os.rename('gis.osm_roads_free_1.dbf', 'osm_roads_free_1.dbf')
+os.rename('gis.osm_roads_free_1.prj', 'osm_roads_free_1.prj')
+os.rename('gis.osm_roads_free_1.shp', 'osm_roads_free_1.shp')
+os.rename('gis.osm_roads_free_1.shx', 'osm_roads_free_1.shx')
+
+# set your CMD environment to run GDAL geoprocessing scripts
+subprocess.call('C:/OSGeo4W64/bin/o4w_env.bat')
+
+# Use ogr2ogr to convert your shapefile to a geopackage
+subprocess.call('C:/OSGeo4W64/bin/ogr2ogr.exe -f GPKG osm_roads.gpkg osm_roads_free_1.shp -sql "SELECT * FROM osm_roads_free_1 WHERE name is not null"')
